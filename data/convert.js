79612eef3a75c7bc92d826457ae9c00fed164754
kekemoto <kekemoto.hp@gmail.com>
2020-01-06 05:34:01 +0900

    CVS変換でキャストを追加

--- data/convert.js
+++ data/convert.js
@@ -5,6 +5,24 @@ let data = fs.readFileSync('./data/cards.csv')
 
 data = parse(data, {columns: true})
 
+// cast
+let tmp = null
+for(c of data){
+	c.value = Number(c.value)
+
+	tmp = toBool(c.defaultField);
+	(tmp === undefined) && castError(c, 'defaultField')
+	c.defaultField = tmp
+
+	tmp = Number(c.sortOrder)
+	Number.isNaN(tmp) && castError(c, 'sortOrder')
+	c.sortOrder = tmp
+
+	c.cost = Number(c.cost)
+	Number.isNaN(tmp) && castError(c, 'cost')
+	c.cost = tmp
+}
+
 data = JSON.stringify(data)
 
 data = `
@@ -14,3 +32,18 @@ export const CARDS = List(${data})
 `
 
 fs.writeFileSync('./data/cards.js', data)
+
+
+function toBool(string){
+	if(/true|True|TRUE/.test(string)){
+		return true
+	} else if (/false|False|FALSE/.test(string)) {
+		return false
+	} else {
+		return undefined
+	}
+}
+
+function castError(card, key){
+	throw new Error(`型変換できません。 ${key}:${card[key]} name:${card.name}`)
+}
